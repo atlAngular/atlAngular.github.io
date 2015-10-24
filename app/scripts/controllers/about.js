e7dde5c8f2e88d17204b961046215cf9e8831d22
tliliachref <achreftlili1990@gmail.com>
2015-10-24 15:47:43 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name atlAngulargithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the atlAngulargithubioApp
+ */
+angular.module('atlAngulargithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name atlAngulargithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the atlAngulargithubioApp
+ */
+angular.module('atlAngulargithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
